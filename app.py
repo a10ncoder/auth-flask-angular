d23f35f801629280f9e964fb89ff8b552abf099b
katai <alonsito79@gmail.com>
2016-11-03 16:42:41 +0100

    Added static file example and routes

--- app.py
+++ app.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello word!'
+    return app.send_static_file('index.html')
+    
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    pass
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    pass
 
 
 if __name__ == '__main__':
